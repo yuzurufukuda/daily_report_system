19b1a628eabc604d6e235fbdf92c1aaf4c2a3bb6
yuduru.fukuda <yuzurufukuda2136@gmail.com>
2021-03-09 17:33:00 +0900

    Add Employee edit

--- WebContent/WEB-INF/views/employees/edit.jsp
+++ WebContent/WEB-INF/views/employees/edit.jsp
@@ -4,19 +4,20 @@
     <c:param name="content">
         <c:choose>
             <c:when test="${employee != null}">
-                <h2>id : ${employee.id} の従業員　編集ページ</h2>
-                <p>(パスワードを変更する場合のみ入力してください)</p>
-                <form method="POST" action="<c:url value='/employee/update' />">
+                <h2>id : ${employee.id} の従業員情報　編集ページ</h2>
+                <p>（パスワードは変更する場合のみ入力してください）</p>
+                <form method="POST" action="<c:url value='/employees/update' />">
                     <c:import url="_form.jsp" />
                 </form>
-                <p><a href="#" onclick="confirmDestroy();">この従業員の情報を削除する</a></p>
+
+                <p><a href="#" onclick="confirmDestroy();">この従業員情報を削除する</a></p>
                 <form method="POST" action="<c:url value='/employees/destroy' />">
                     <input type="hidden" name="_token" value="${_token}" />
                 </form>
                 <script>
-                    function confirmDestroyU(){
-                        if(confirm("本当に削除してよろしいですか？")){
-                            doucument.forms[1].submit();
+                    function confirmDestroy() {
+                        if(confirm("本当に削除してよろしいですか？")) {
+                            document.forms[1].submit();
                         }
                     }
                 </script>
@@ -25,6 +26,7 @@
                 <h2>お探しのデータは見つかりませんでした。</h2>
             </c:otherwise>
         </c:choose>
-        <p><a href="<c:url value='/employee/index' />">一覧に戻る</a></p>
+
+        <p><a href="<c:url value='/employees/index' />">一覧に戻る</a></p>
     </c:param>
 </c:import>
--- src/controllers/employees/EmployeesUpdateServlet.java
+++ src/controllers/employees/EmployeesUpdateServlet.java
@@ -16,6 +16,7 @@ import models.Employee;
 import models.validators.EmployeeValidator;
 import utils.DBUtil;
 import utils.EncryptUtil;
+
 /**
  * Servlet implementation class EmployeesUpdateServlet
  */
@@ -35,16 +36,15 @@ public class EmployeesUpdateServlet extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String _token = (String)request.getParameter("_token");
-        if(_token != null && _token.equals(request.getSession().getId())){
+        if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
             Employee e = em.find(Employee.class, (Integer)(request.getSession().getAttribute("employee_id")));
 
-            // 現在の値と異なる社員番号は入力されていたら
+            // 現在の値と異なる社員番号が入力されていたら
             // 重複チェックを行う指定をする
             Boolean codeDuplicateCheckFlag = true;
-
-            if(e.getCode().equals(request.getParameter("code"))){
+            if(e.getCode().equals(request.getParameter("code"))) {
                 codeDuplicateCheckFlag = false;
             } else {
                 e.setCode(request.getParameter("code"));
@@ -54,37 +54,41 @@ public class EmployeesUpdateServlet extends HttpServlet {
             // パスワードの入力値チェックを行う指定をする
             Boolean passwordCheckFlag = true;
             String password = request.getParameter("password");
-            if(password == null || password.equals("")){
+            if(password == null || password.equals("")) {
                 passwordCheckFlag = false;
             } else {
                 e.setPassword(
                         EncryptUtil.getPasswordEncrypt(
                                 password,
                                 (String)this.getServletContext().getAttribute("pepper")
-                        )
-                );
-                e.setName(request.getParameter("name"));
-                e.setAdmin_flag(Integer.parseInt(request.getParameter("admin_flag")));
-                e.setUpdated_at(new Timestamp(System.currentTimeMillis()));
-                e.setDelete_flag(0);
+                                )
+                        );
+            }
 
-                List<String> errors = EmployeeValidator.validate(e, codeDuplicateCheckFlag, passwordCheckFlag);
-                if(errors.size() > 0){
-                    em.close();
+            e.setName(request.getParameter("name"));
+            e.setAdmin_flag(Integer.parseInt(request.getParameter("admin_flag")));
+            e.setUpdated_at(new Timestamp(System.currentTimeMillis()));
+            e.setDelete_flag(0);
 
-                    request.setAttribute("_token", request.getSession().getId());
-                    request.setAttribute("employee", e);
-                    request.setAttribute("errors", errors);
+            List<String> errors = EmployeeValidator.validate(e, codeDuplicateCheckFlag, passwordCheckFlag);
+            if(errors.size() > 0) {
+                em.close();
 
-                    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employee/edit.jsp");
-                    rd.forward(request, response);
-                } else {
-                    em.getTransaction().begin();
-                    em.getTransaction().commit();
-                    em.close();
-                    request.getSession().removeAttribute("employee_id");
-                    response.sendRedirect(request.getContextPath() + "/employees/index");
-                }
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("employee", e);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/edit.jsp");
+                rd.forward(request, response);
+            } else {
+                em.getTransaction().begin();
+                em.getTransaction().commit();
+                em.close();
+                request.getSession().setAttribute("flush", "更新が完了しました。");
+
+                request.getSession().removeAttribute("employee_id");
+
+                response.sendRedirect(request.getContextPath() + "/employees/index");
             }
         }
     }
